0b1aee694519c7fe410fe65a42e830f443fbaa65
yiu-code <kimyiu-lui@hotmail.com>
2025-05-25 18:12:43 +0200

    fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/icon'],
+  ssr: false,
 
   // Base URL for GitHub Pages (replace repository name)
   app: {
-    baseURL: '/'
+    baseURL: '/website'
   },
 
   // Enable strict TypeScript checks (optional but recommended)
   typescript: {
     strict: true
+  },
+
+  nitro: {
+    preset: 'static'
   }
 })
